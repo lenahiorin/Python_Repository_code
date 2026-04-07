74f58bbc8021908edb06aeb882a5b98b581f48b1
lenahiorin <aliyalenasanoubane@gmail.com>
2026-04-07 18:53:00 +0700

    Add files via upload

---
+++ Bithday.py
@@ -0,0 +1,11 @@
+birthday1 = "January 5"
+birthday2 = "March 12"
+birthday3 = "June 30"
+birthday4 = "September 18"
+birthday5 = "December 25"
+
+print(birthday1)
+print(birthday2)
+print(birthday3)
+print(birthday4)
+print(birthday5)
